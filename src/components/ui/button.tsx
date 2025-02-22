7e8eaf4451c0cbc30c01175e9553cd1f7a8f9b74
andreo141 <andreo141mlg@gmail.com>
2025-02-22 02:15:13 +0100

    Basic navbar and routing

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,53 @@
+import { cva } from "class-variance-authority";
+import type { VariantProps } from "class-variance-authority"; // Type-only import
+import React from "react";
+import type { ButtonHTMLAttributes } from "react";
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva("font-head transition-all outline-none", {
+  variants: {
+    variant: {
+      default:
+        "shadow-md hover:shadow-xs bg-primary-400 text-black border-2 border-black hover:bg-primary-500",
+      outline:
+        "shadow-md hover:shadow-xs bg-transparent text-black border-2 border-black",
+      link: "bg-transparent text-black hover:underline",
+    },
+    size: {
+      sm: "px-4 py-1 text-sm",
+      md: "px-6 py-2 text-base",
+      lg: "px-8 py-3 text-lg",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+    variant: "default",
+  },
+});
+
+export interface IButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+export const Button = React.forwardRef<HTMLButtonElement, IButtonProps>(
+  (
+    {
+      children,
+      size = "md",
+      className = "",
+      variant = "default",
+      ...props
+    }: IButtonProps,
+    forwardedRef
+  ) => (
+    <button
+      ref={forwardedRef}
+      className={cn(buttonVariants({ variant, size }), className)}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+);
+
+Button.displayName = "Button";
