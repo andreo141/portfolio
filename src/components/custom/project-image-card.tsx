c59c967825d3a77cf9df2c68df88fccbe4098ecf
andreo141 <andreo141mlg@gmail.com>
2025-05-21 03:38:01 +0200

    ui fixes

--- src/components/custom/project-image-card.tsx
+++ src/components/custom/project-image-card.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function ImageCard({ imageUrl, caption }: Props) {
   return (
-    <figure className="w-[400px] overflow-hidden rounded-base border-2 border-border bg-main font-base shadow-shadow">
+    <figure className="w-[400px] overflow-hidden rounded-xs border-2 border-border bg-main font-base shadow-shadow">
       <img className="w-full aspect-[4/3]" src={imageUrl} alt="image" />
     </figure>
   );
