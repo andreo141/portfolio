bf467491d0e17a6f599662b7b41c45c1dbf75e0f
andreo141 <andreo141mlg@gmail.com>
2025-09-24 03:06:15 +0200

    WIP: responsive design + about section revamp

--- src/components/ui/image-card.tsx
+++ src/components/ui/image-card.tsx
@@ -1,13 +1,16 @@
 type Props = {
   imageUrl: string;
-  caption: string;
+  caption?: string;
+  className?: string;
 };
 
-export default function ImageCard({ imageUrl, caption }: Props) {
+export default function ImageCard({ imageUrl, caption, className }: Props) {
   return (
-    <figure className="w-[250px] overflow-hidden rounded-base border-2 border-border bg-main font-base shadow-shadow">
-      <img className="w-full aspect-[4/3]" src={imageUrl} alt="image" />
-      <figcaption className="border-t-2 text-mtext border-border p-4 text-center">
+    <figure
+      className={`${className} overflow-hidden rounded-xs border-2 border-border bg-main font-base shadow-shadow`}
+    >
+      <img className="w-full aspect-square" src={imageUrl} alt="image" />
+      <figcaption className="border-t-2 text-mtext border-border text-center">
         {caption}
       </figcaption>
     </figure>
